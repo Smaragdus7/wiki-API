fcb218ff51bff8e3577842e2a8537859d5e5d2a4
Smaragdus7 <smar.leeft@gmail.com>
2023-02-08 16:46:59 -0600

    CRUD 2

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-//Connection
+//DB Connection
 mongoose.connect('mongodb://127.0.0.1:27017/wikiDB');
 mongoose.set('strictQuery', false);
 
@@ -24,17 +24,33 @@ const articleSchema = new mongoose.Schema({
 //Model
 const Article = mongoose.model('Article', articleSchema);
 
+// C R U D    O P E R A T I O N S
+
+//Get all articles
 app.get('/articles', (req, res) => { 
-    Article.find({}, function(err, foundArticles) {
-        if(!err) {
-            res.send(foundArticles);
-        }
-        else {
-            res.send(err);
-        }  
+  Article.find({}, function(err, foundArticles) {
+    if(!err) {
+      res.send(foundArticles);
+    }
+    else {
+      res.send(err);
+    }  
   });
 });
 
+//Get a specific article
+app.get('/articles/:id', (req, res) => { 
+  Article.findOne({_id: req.params.id}, function(err, foundArticle) {
+    if(!err) {
+      res.send(foundArticle);
+    }
+    else {
+      res.send(err);
+    }  
+  });
+});
+
+//Post an article
 app.post('/articles', (req, res) => {
   const newArticle = Article({
     title: req.body.title,
@@ -51,15 +67,48 @@ app.post('/articles', (req, res) => {
   });
 });
 
+//Delete all articles
 app.delete('/articles', (req, res) => {
   Article.deleteMany(function(err){
     if(!err) {
-      res.send("Successfully deleted!");
+      res.send("Articles Successfully deleted!");
     }
     else {
       res.send(err);
     }
   });
+});
+
+//Delete a specific article***********
+app.delete('/articles/:id', (req, res) => {
+  Article.findByIdAndRemove({_id: req.params.id}, function(err) {
+    if(!err) {
+      res.send("Article Successfully deleted!");
+    }
+    else {
+      res.send(err);
+    }
+  });
+});
+
+//Put a specific article******
+app.put("/articles/:id", async (req, res) => {
+  const { id } = req.params;
+  Article.updateOne({ id }, req.body);
+  const updatedArticle = Article.findById(id);
+  return res.status(200).json(updatedArticle);
+});
+
+//Path a specific article*********
+app.patch('/articles/:id', (req, res) => {
+  Article.findByIdAndUpdate(req.params.id, req.body, {new: true}).then((article) => {
+    if (!article) {
+      return res.status(404).send();
+    }
+    res.send(article);
+  }).catch((error) => {
+    res.status(500).send(error);
+  })
 });
 
 app.listen(port, () => {
